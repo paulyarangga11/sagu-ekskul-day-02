9344ab0ca8e07d70bee75ab2db98aced5d56153f
PAUL M YARANGGA <paulyarangga07@gmail.com>
2022-02-26 09:31:47 +0700

    push project 03

---
+++ project03/comments.php
@@ -0,0 +1,10 @@
+<?php
+    //this is my first single line comment in PHP
+    echo "single line comment using '//' <br>"; 
+
+    # this is also single line comment
+    echo "single line comment using '#' <br> ";
+
+    /* this is multiple line comment in PHP */
+    echo "multiple line comment in PHP using '/*...*/' ";
+?>
